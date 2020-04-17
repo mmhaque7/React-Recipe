cc5c705e3bb29e677bf8c449ab3f194a4ac66f00
Nirav <nirav@alfangroup.com>
2020-04-17 10:22:45 -0400

    refactored a bit

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,10 +1,15 @@
 import React from "react";
 
-const RecipeList = (props) => {
-  const recipes = recipes.props.map((recipe) => {
-    return <div key={recipe.id}>Name</div>;
-  });
-  return <div className="recipe-list">{recipes}</div>;
+const RecipeList = ({ recipes }) => {
+  const renderRecipes = () => {
+    if (!recipes) return;
+
+    return recipes.map((recipe) => {
+      return <div key={recipe.id}>{recipe.name}</div>;
+    });
+  };
+
+  return <div className="recipe-list">{renderRecipes()}</div>;
 };
 
 export default RecipeList;
